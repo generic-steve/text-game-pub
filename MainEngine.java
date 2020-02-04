73ee6f98f1d8a752f5eb2ef3e53a3a8749f9453d
C.Hill <2024hillc03@moundsviewschools.org>
2020-02-04 16:00:42 -0600

    Fixed rendering class

---
+++ MainEngine.java
@@ -0,0 +1,7 @@
+import Main.Constants;
+
+public class MainEngine{
+    public static void main(String[] args){
+        RenderEngine.Frame(Constants.testStage, Constants.testNumList, Constants.testNumList, Constants.testSprite);
+    }
+}
