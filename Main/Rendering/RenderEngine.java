5ef2945f0de41a8272b34963b2a0d1ef7c6722d8
null <null>
2020-02-11 17:42:56 -0600

    Made it not add a sprite if it goes outside the boundaries

--- Main/Rendering/RenderEngine.java
+++ Main/Rendering/RenderEngine.java
@@ -1,6 +1,8 @@
 public class RenderEngine {
-    public static void Frame(String[][] stage, int[] spritex, int[] spritey, String[][][] sprites){
+    public static void init(){
         GameFrame.NewFrame();
+    }
+    public static void Frame (String[][] stage, int[] spritex, int[] spritey, String[][][] sprites){
         String[][] frame = stage;
         for(int i = 0; (i) < (sprites.length); i++) {
             frame = spriteAdd(spritex[i], spritey[i], sprites[i], frame);
@@ -15,6 +17,9 @@ public class RenderEngine {
     public static String[][] spriteAdd(int spritex, int spritey, String[][] sprite, String[][] frame) {
         final int spritePhysicalX = sprite.length;
         final int spritePhysicalY = sprite[1].length;
+        if (spritePhysicalY + sprite[1].length > frame[1].length || spritePhysicalX + sprite.length > frame.length){
+            return frame;
+        }
         for (int x = 0; (x) < spritePhysicalX; x++){
             for (int y = 0; (y) < spritePhysicalY; y++) {
                 frame[spritePhysicalX + x][spritePhysicalY + y] = sprite[x][y];
