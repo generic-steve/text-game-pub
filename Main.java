3e2f9f6a612028847a1134d58f8a00e7a0dfb81f
Caleb Hill <2024hillc03@moundsviewschools.org>
2020-03-19 09:02:17 -0500

    implemented double buffering

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 import Main.Constants;
+import Main.Rendering.GameFrame;
 import Main.Rendering.RenderEngine;
 import Main.Types.Entity;
-public class Main{
+
+public class Main {
     public static void main(String[] args){
         Entity Ent1 = new Entity(15, 35, Constants.TestSprite);
         Entity[] EntList1 = {Ent1};
-        RenderEngine.init();
-        RenderEngine.Frame(Constants.TestStage, EntList1);//TODO when adding multiple frames, remember to call BufferFlip in RenderEngine
+        GameFrame MainFrame = RenderEngine.init();
+        while(true){
+            RenderEngine.Frame(Constants.TestStage, EntList1, MainFrame);//TODO when adding multiple frames, remember to call BufferFlip in RenderEngine
+        //To gus: updte entlist using keypresses here.
+        }
     }
 }
--- Main/Rendering/RenderEngine.java
+++ Main/Rendering/RenderEngine.java
@@ -4,10 +4,10 @@ import Main.Types.Entity;
 
 public class RenderEngine {
     static boolean DoubleBufferFlipFlop = true;
-    public static void init(){
-        GameFrame.NewFrame();
+    public static GameFrame init(){
+        return GameFrame.NewFrame();
     }
-    public static void Frame (String[][] stage, Entity[] entList){
+    public static void Frame (String[][] stage, Entity[] entList, GameFrame Frame1){
         DoubleBufferFlipFlop = !DoubleBufferFlipFlop;
         String[][] frame = stage;
         for(int i = 0; (i) < (entList.length); i++) {
@@ -22,22 +22,26 @@ public class RenderEngine {
                 }
             }
         }
+        BufferFlip(DoubleBufferFlipFlop, Frame1);
     }
-    //TODO fix error when subarrays are different lengths, or just don't do that. note: many other classes also crash when this happens
+
+    // TODO fix error when subarrays are different lengths, or just don't do that.
+    // note: many other classes also crash when this happens
     public static String[][] spriteAdd(int spritex, int spritey, String[][] sprite, String[][] frame) {
         int spritePhysicalX = sprite.length;
         int spritePhysicalY = sprite[0].length;
-        if (spritePhysicalY + spritey > frame[1].length || spritePhysicalX + spritex > frame.length){
+        if (spritePhysicalY + spritey > frame[1].length || spritePhysicalX + spritex > frame.length) {
             return frame;
         }
-        for (int x = 0; x < spritePhysicalX; x++){
+        for (int x = 0; x < spritePhysicalX; x++) {
             for (int y = 0; y < spritePhysicalY; y++) {
                 frame[spritex + x][spritey + y] = sprite[x][y];
             }
         }
         return frame;
     }
-    public void BufferFlip(boolean FlipState, GameFrame Frame1){
+
+    public static void BufferFlip(boolean FlipState, GameFrame Frame1) {
         for (int x = 0; x < 20; x++) {
             for (int y = 0; y < 40; y++) {
                 if(FlipState){
